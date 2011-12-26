54fc2b6bca2204d4c3fecb4a23f9b1e5528de573
James Paolantonio <james@jdubstudios.com>
2011-12-26 16:15:42 -0500

    Renamed JPValidationStatus

---
+++ JPValidators/JPBaseValidator.h
@@ -0,0 +1,13 @@
+//
+//  JPBaseValidator.h
+//  Yumm
+//
+//  Created by James Paolantonio on 12/26/11.
+//  Copyright (c) 2011 JDub Studios, llc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPBaseValidator : NSObject
+
+@end
